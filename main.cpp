9308b866e3c33a28fb7409de2d0e0a55a9e0ade5
Marcin Rał <marak89@marak89.com>
2018-01-06 14:44:56 +0100

    l1

--- main.cpp
+++ main.cpp
@@ -1,11 +1,27 @@
-#include "mainwindow.h"
+//#include "mainwindow.h"
 #include <QApplication>
-
+#include <QGraphicsScene>
+#include <QGraphicsRectItem>
+#include <QGraphicsView>
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    MainWindow w;
-    w.show();
+    //MainWindow w;
+    //w.show();
+
+    // scena
+    QGraphicsScene * scene = new QGraphicsScene();
+
+    // item na scene
+    QGraphicsRectItem * rect = new QGraphicsRectItem();
+    rect->setRect(0,0,100,100);
+
+    scene->addItem(rect);
+
+    // add a view
+    QGraphicsView * view = new QGraphicsView(scene);
+    view->show();
+
 
     return a.exec();
 }
